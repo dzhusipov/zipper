de7256b112cd577b7e9f1dc758d1e90a8d1139b8
Жусипов Даурхан Асханович <Daurkhan.Zhussipov@kaspi.kz>
2022-06-08 14:25:45 +0600

    run fask

--- main.py
+++ main.py
@@ -77,6 +77,6 @@ def archive(file_name):
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=5000)
